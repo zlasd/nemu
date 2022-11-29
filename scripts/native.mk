d42a40f553c3e0cab47365b5a66e37028397c49e
zlasd <zlasd@hotmail.com>
2022-11-29 13:01:17 +0000

    remove auto commit

--- scripts/native.mk
+++ scripts/native.mk
@@ -4,7 +4,6 @@ include $(NEMU_HOME)/scripts/build.mk
 include $(NEMU_HOME)/tools/difftest.mk
 
 compile_git:
-	$(call git_commit, "compile")
 $(BINARY): compile_git
 
 # Some convenient rules
@@ -19,11 +18,9 @@ NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 run-env: $(BINARY) $(DIFF_REF_SO)
 
 run: run-env
-	$(call git_commit, "run")
 	$(NEMU_EXEC)
 
 gdb: run-env
-	$(call git_commit, "gdb")
 	gdb -s $(BINARY) --args $(NEMU_EXEC)
 
 clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
