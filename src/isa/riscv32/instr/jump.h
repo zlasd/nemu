0480e4db48191d46de59306973b10f79b718404e
zlasd <zlasd@hotmail.com>
2023-01-17 06:39:14 +0000

    feat: implement jalr & ebreak

--- src/isa/riscv32/instr/jump.h
+++ src/isa/riscv32/instr/jump.h
@@ -2,3 +2,12 @@ def_EHelper(jal) {
   rtl_addi(s, ddest, &s->pc, 4);
   rtl_addi(s, &s->dnpc, &s->pc, id_src1->imm);
 }
+
+def_EHelper(jalr) {
+  rtl_addi(s, ddest, &s->pc, 4);
+  rtl_addi(s, &s->dnpc, dsrc1, id_src2->imm);
+}
+
+def_EHelper(ebreak) {
+  rtl_hostcall(s, HOSTCALL_EXIT, NULL, &gpr(10), NULL, 0); // gpr(10) is $a0
+}
