3b034a7a55326afed3ba2bc455890e6424e1a2dc
zlasd <zlasd@hotmail.com>
2023-01-17 04:01:47 +0000

    feat: implement auipc & jal

--- src/isa/riscv32/instr/compute.h
+++ src/isa/riscv32/instr/compute.h
@@ -2,6 +2,11 @@ def_EHelper(lui) {
   rtl_li(s, ddest, id_src1->imm);
 }
 
+def_EHelper(auipc) {
+  rtl_li(s, ddest, id_src1->imm);
+  rtl_add(s, ddest, &s->dnpc, ddest);
+}
+
 def_EHelper(addi) {
   rtl_addi(s, ddest, dsrc1, id_src2->imm);
 }
---
+++ src/isa/riscv32/instr/jump.h
@@ -0,0 +1,4 @@
+def_EHelper(jal) {
+  rtl_addi(s, ddest, &s->pc, 4);
+  rtl_addi(s, &s->dnpc, &s->pc, id_src1->imm);
+}
